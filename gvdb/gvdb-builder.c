c70e06b42823950d7adeabf100cba6a004f9199f
Emmanuele Bassi <ebassi@gnome.org>
2023-04-13 13:29:14 +0100

    Add getter for GVDB table contents
    The xdg-desktop-portal project is using GVDB as the basis for the
    permissions store.
    
    Each permission table is stores as a block of bytes inside another GVDB
    table, in order to isolate the permissions of each application.
    
    Currently, xdg-desktop-portal uses GVDB as a copy-paste library; the
    only missing API to avoid that, and switch GVDB to a proper subproject,
    is a way to extract the contents of a GVDB table into a GBytes buffer.

--- gvdb/gvdb-builder.c
+++ gvdb/gvdb-builder.c
@@ -509,28 +509,44 @@ file_builder_serialise (FileBuilder          *fb,
   return result;
 }
 
+GBytes *
+gvdb_table_get_contents (GHashTable     *table,
+                         gboolean        byteswap)
+{
+  struct gvdb_pointer root;
+  FileBuilder *fb;
+  GString *str;
+  GBytes *res;
+  gsize str_len;
+
+  fb = file_builder_new (byteswap);
+  file_builder_add_hash (fb, table, &root);
+  str = file_builder_serialise (fb, root);
+
+  str_len = str->len;
+  res = g_bytes_new_take (g_string_free (str, FALSE), str_len);
+
+  return res;
+}
+
 gboolean
 gvdb_table_write_contents (GHashTable   *table,
                            const gchar  *filename,
                            gboolean      byteswap,
                            GError      **error)
 {
-  struct gvdb_pointer root;
+  GBytes *content;
   gboolean status;
-  FileBuilder *fb;
-  GString *str;
 
   g_return_val_if_fail (table != NULL, FALSE);
   g_return_val_if_fail (filename != NULL, FALSE);
   g_return_val_if_fail (error == NULL || *error == NULL, FALSE);
 
-  fb = file_builder_new (byteswap);
-  file_builder_add_hash (fb, table, &root);
-  str = file_builder_serialise (fb, root);
-  file_builder_free (fb);
+  content = gvdb_table_get_contents (table, byteswap);
 
-  status = g_file_set_contents (filename, str->str, str->len, error);
-  g_string_free (str, TRUE);
+  status = g_file_set_contents (filename, g_bytes_get_data (content, NULL), g_bytes_get_size (content), error);
+
+  g_bytes_unref (content);
 
   return status;
 }
